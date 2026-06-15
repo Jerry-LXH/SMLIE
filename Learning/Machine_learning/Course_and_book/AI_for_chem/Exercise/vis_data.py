f1729fbfd79ef7a9b690f9ba4ab3aa682df0c386
JerryLing <lingxinghao140@gmail.com>
2026-06-15 17:17:15 +0800

    Remove large files and cleanup

--- Learning/Machine_learning/Course_and_book/AI_for_chem/Exercise/vis_data.py
+++
@@ -1,8 +0,0 @@
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-def show_hist(data):
-    mpl.rc('axes', labelsize=14) # axis font size
-    mpl.rc('xtick', labelsize=12)# x,y-label font size
-    mpl.rc('ytick', labelsize=12) # 全局图片生成样式设置
-    data.hist(bins=50, figsize=(20,15))
-    plt.show()
--- Projects/interest/pipeAuto/datasets/Pipe_Defects_Public/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# Pipe Defects > DrainPipes_v1
-https://universe.roboflow.com/pipedefectscomputervision/pipe-defects-howis
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
